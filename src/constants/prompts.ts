d461b033ecb6bdbd5aab8b482a9bb3901ee93d33
Nuno Rocha <galaxyroch@gmail.com>
2025-11-12 20:17:40 +0000

    feat: Limit the number of SERP queries to a maximum of 10 in report generation

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -124,7 +124,7 @@ export const serpQueriesPrompt = `This is the report plan after user confirmatio
 {plan}
 </PLAN>
 
-Based on previous report plan, generate a list of SERP queries to further research the topic. Make sure each query is unique and not similar to each other.
+Based on previous report plan, generate a list of SERP queries to further research the topic. Make sure each query is unique and not similar to each other. Provide a max of 10 queries.
 
 ${serpQuerySchemaPrompt}`;
 
